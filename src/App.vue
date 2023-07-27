a2fbf86e9281c16e6ce842518ff9476b80311598
KarlCrisostomo12 <101928489+KarlCrisostomo12@users.noreply.github.com>
2023-07-27 18:58:37 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-black font-Poppins">
+  <div class=" bg-black font-Poppins">
     <div
       :class="{
         ' w-8 h-8 fixed top-0 left-0 border-2 border-white border-solid rounded-full z-50 pointer-events-none':
@@ -11,7 +11,7 @@
     ></div>
 
     <router-view />
-  </div>
+</div>
 </template>
 
 <style>
